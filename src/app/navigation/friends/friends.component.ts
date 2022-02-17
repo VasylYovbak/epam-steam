8880ff44f6b8f06c442f410dc886b3e9d7f6dd6f
Petro Loi <drowdrake@gmail.com>
2022-02-17 12:47:48 +0200

    friends component changes

--- src/app/navigation/friends/friends.component.ts
+++ src/app/navigation/friends/friends.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../services/user.model';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-friends',
   templateUrl: './friends.component.html',
-  styleUrls: ['./friends.component.scss']
+  styleUrls: ['./friends.component.scss'],
 })
 export class FriendsComponent implements OnInit {
+  users: User[] = [];
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    this.users = this.userService.getUsers();
   }
 
+  isFriends(): boolean {
+    return this.users[0].friends.length !== 0;
+  }
+
+  usersFriends(): string[] {
+    return this.users[0].friends.map((user) => user);
+  }
+
+  onAddFriend(item: User) {
+    this.userService.addToUserCollection(item);
+  }
+
+  onRemoveFriend(userName: User): void {
+    console.log(userName)
+    // this.userService.removeFromUserCollection(userName);
+    // console.log(this.users[0].friends.filter((user) => user.indexOf(userName)));
+  }
 }
